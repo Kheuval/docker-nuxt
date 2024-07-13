52c0fdfcfdafe12448764ed0e183dfdd1ff6c070
Kheuval <hadrien.hartstein@gmail.com>
2024-07-13 09:14:33 +0200

    Working for dev

--- apps/nuxt/nuxt.config.ts
+++ apps/nuxt/nuxt.config.ts
@@ -1,5 +1,22 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  vite: { // @see https://github.com/nuxt/nuxt/issues/27558
+    server: {
+      hmr: {
+        protocol: "wss",
+        clientPort: 443,
+        path: "hmr/",
+      },
+    },
+  },
+  hooks: { // @see https://github.com/nuxt/nuxt/issues/27558
+    'vite:extendConfig': (config) => {
+      if (typeof config.server?.hmr === 'object') {
+        config.server.hmr.protocol = 'wss';
+        config.server.hmr.clientPort = 443;
+        config.server.hmr.path = "hmr/";
+      }
+    },
+  },
 })
